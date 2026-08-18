c70478b9a934cb3dc9d7b6d6c9f03d3533a5f272
Charlie <charliegrillo@gmail.com>
2026-08-18 17:30:30 -0400

    fix:Fix select

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  allowedDevOrigins: ['172.16.0.65'],
+  allowedDevOrigins: ['172.16.0.65','192.168.1.101'],
 }
 
 export default nextConfig
